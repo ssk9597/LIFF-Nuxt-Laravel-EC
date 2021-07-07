e66b5f533bd9019315c825a7a081d19d6221f9fa
sasaki <skyo.97.admj@gmail.com>
2021-07-07 18:39:14 +0900

    【Frontend】Create an image

--- frontend/components/Atoms/Image.vue
+++ frontend/components/Atoms/Image.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="image">
-    <img class="hero-img" src="@/assets/images/top.png" alt="ヒーロー" />
     <img src="@/assets/images/line.png" alt="LINEログイン" />
   </div>
 </template>
@@ -9,4 +8,14 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.image {
+  width: 100%;
+  height: 100%;
+
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
