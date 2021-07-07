104fa767d91986f73c8d784d44b8ef2bf5fdbbd8
sasaki <skyo.97.admj@gmail.com>
2021-07-07 13:46:04 +0900

    git init

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const { API_URL } = process.env;
+
+export default {
+  target: 'static',
+  head: {
+    title: 'frontend',
+    htmlAttrs: {
+      lang: 'ja',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [],
+
+  watchers: {
+    webpack: {
+      poll: true,
+    },
+  },
+
+  modules: ['@nuxtjs/axios', '@nuxtjs/proxy', '@nuxtjs/dotenv'],
+
+  env: {
+    API_URL,
+  },
+
+  proxy: {
+    '/api': process.env.API_URL,
+  },
+
+  axios: {
+    baseURL: process.env.API_URL,
+    browserBaseURL: process.env.API_URL,
+  },
+
+  build: {},
+};
