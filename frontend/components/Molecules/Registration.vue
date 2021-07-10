40cd681bebd451636a3d0bb5fe0a195d4e9baa93
sasaki <skyo.97.admj@gmail.com>
2021-07-10 09:35:48 +0900

    Add text so that you can get an email address

--- frontend/components/Molecules/Registration.vue
+++ frontend/components/Molecules/Registration.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="registration">
     <Heading :main="'Registration'" :sub="'会員登録'" />
-
-    <div class="image">
-      <img src="@/assets/images/line.png" alt="LINEログイン" @click="childLineLogin" />
+    <div class="registration-container">
+      <p class="registration-container-text">
+        本Webサービスでは、ログイン時の認証画面にて許可を頂いた場合のみ、あなたのLINEアカウントに登録されているメールアドレスを取得します。
+      </p>
+      <p class="registration-container-text">
+        取得したメールアドレスは、HubSpotにユーザー登録するIDとして利用する目的以外では使用いたしません。また、法令に定められた場合を除き、第三者への提供はいたしません。
+      </p>
+      <div class="registration-container-image">
+        <img src="@/assets/images/line.png" alt="LINEログイン" @click="childLineLogin" />
+      </div>
     </div>
   </div>
 </template>
@@ -30,7 +37,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.image {
-  text-align: center;
+.registration-container {
+  width: 250px;
+  margin: 0 auto;
+  background: $color_white;
+  padding: 20px;
+
+  &-text {
+    font-size: 0.5rem;
+    line-height: 1.5;
+    padding-bottom: 10px;
+  }
+
+  &-image {
+    padding-top: 20px;
+    text-align: center;
+  }
 }
 </style>
