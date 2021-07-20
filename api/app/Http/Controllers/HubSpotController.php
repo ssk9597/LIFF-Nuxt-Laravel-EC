030c66a15606b8b4d926dc8cf4c8e9da5b4afcff
sasaki <skyo.97.admj@gmail.com>
2021-07-20 09:58:58 +0900

    Debug

--- api/app/Http/Controllers/HubSpotController.php
+++ api/app/Http/Controllers/HubSpotController.php
@@ -9,25 +9,29 @@ class HubSpotController extends Controller
 {
   public function store(Request $request)
   {
-    // request
-    $idToken = $request->input("idToken");
+    try {
+      // request
+      $idToken = $request->input("idToken");
 
-    // env
-    // $clientID = config("env.line_client_id");
-    $clientID = "1656187153";
+      // env
+      // $clientID = config("env.line_client_id");
+      $clientID = "1656187153";
 
-    // guzzle
-    $client = new Client();
-    $response = $client->request("POST", "https://api.line.me/oauth2/v2.1/verify", [
-      "form_params" => [
-        "id_token" => $idToken,
-        "client_id" => $clientID
-      ]
-    ]);
+      // guzzle
+      $client = new Client();
+      $response = $client->request("POST", "https://api.line.me/oauth2/v2.1/verify", [
+        "form_params" => [
+          "id_token" => $idToken,
+          "client_id" => $clientID
+        ]
+      ]);
 
-    $name = $response->name;
-    $email = $response->email;
+      $name = $response->name;
+      $email = $response->email;
 
-    return $name;
+      return $name;
+    } catch (\Exception $e) {
+      return $e;
+    }
   }
 }
