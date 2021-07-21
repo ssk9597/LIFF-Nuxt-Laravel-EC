f1ccf6dfe38ee9102acdba92013b7d0ec729ab4b
sasaki <skyo.97.admj@gmail.com>
2021-07-22 08:18:10 +0900

    cURL

--- api/app/Http/Controllers/HubSpotController.php
+++ api/app/Http/Controllers/HubSpotController.php
@@ -36,25 +36,54 @@ class HubSpotController extends Controller
       Log::info($profile["name"]);
       Log::info($profile["email"]);
 
-      $data = array(
-        "properties" => [
-          "email" => $profileEmail,
-          "firstname" => $profileName
-        ]
-      );
 
-      $formParams = json_encode($data, JSON_UNESCAPED_UNICODE);
+      // Register HubSpot
+      // $data = array(
+      //   "properties" => [
+      //     "email" => $profileEmail,
+      //     "firstname" => $profileName
+      //   ]
+      // );
 
-      Log::info($formParams);
+      // $formParams = json_encode($data, JSON_UNESCAPED_UNICODE);
 
-      $hubspot = $client->request("POST", "https://api.hubapi.com/crm/v3/objects/contacts?hapikey=" . $HubSpotApiKey, [
-        "headers" => [
-          'content-type' => 'application/json'
-        ],
-        'form_params' => [$formParams]
-      ]);
+      // Log::info($formParams);
 
-      Log::info($hubspot);
+      // $hubspot = $client->request("POST", "https://api.hubapi.com/crm/v3/objects/contacts?hapikey=" . $HubSpotApiKey, [
+      //   "headers" => [
+      //     'content-type' => 'application/json'
+      //   ],
+      //   'form_params' => [$formParams]
+      // ]);
+
+      // Log::info($hubspot);
+
+      $curl = curl_init();
+
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.hubapi.com/crm/v3/objects/contacts?hapikey=" . $HubSpotApiKey,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => "{\"properties\":{\"email\":$profileEmail,\"firstname\":$profileName}}",
+        CURLOPT_HTTPHEADER => array(
+          "accept: application/json",
+          "content-type: application/json"
+        ),
+      ));
+
+      $response = curl_exec($curl);
+      $err = curl_error($curl);
+      curl_close($curl);
+
+      if ($err) {
+        Log::info($err);
+      } else {
+        Log::info($response);
+      }
     } catch (\GuzzleHttp\Exception\BadResponseException $e) {
       Log::info($e);
       return $e->getResponse()->getBody()->getContents();
