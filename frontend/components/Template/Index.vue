179c297339e1dad7034042e7171e49f2bbc0e417
sasaki <skyo.97.admj@gmail.com>
2021-07-22 19:52:27 +0900

    no message

--- frontend/components/Template/Index.vue
+++ frontend/components/Template/Index.vue
@@ -30,28 +30,34 @@ export default {
   methods: {
     async lineLogin() {
       try {
-        // await liff
-        //   .init({
-        //     liffId: process.env.LIFF_ID,
-        //   })
-        //   .then(async () => {
-        //     // IDトークン
-        alert(liff.getOS());
-        alert(liff.isLoggedIn());
-        const idToken = liff.getIDToken();
-        alert(idToken);
-        if (idToken) {
-          await this.$axios.$post('/hubspot/store', {
-            idToken: idToken,
+        await liff.sendMessages([
+          {
+            type: 'text',
+            text: 'OK',
+          },
+        ]);
+        await liff
+          .init({
+            liffId: process.env.LIFF_ID,
+          })
+          .then(async () => {
+            // IDトークン
+            alert(liff.getOS());
+            alert(liff.isLoggedIn());
+            const idToken = liff.getIDToken();
+            alert(idToken);
+            if (idToken) {
+              await this.$axios.$post('/hubspot/store', {
+                idToken: idToken,
+              });
+            } else {
+              alert('ログイン失敗しました。もう一度お試しください。');
+            }
+          })
+          .catch((err) => {
+            alert(err.code);
+            alert(err.message);
           });
-        } else {
-          alert('ログイン失敗しました。もう一度お試しください。');
-        }
-        // })
-        // .catch((err) => {
-        //   alert(err.code);
-        //   alert(err.message);
-        // });
       } catch (err) {
         alert(err);
         alert(err.response.data.error);
