90f6c0b912223e08fd2cfc3dd5723167838e8f79
sasaki <skyo.97.admj@gmail.com>
2021-07-27 11:25:05 +0900

    Update payload

--- api/app/Http/Controllers/PayloadController.php
+++ api/app/Http/Controllers/PayloadController.php
@@ -3,27 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Stripe\Stripe;
 use Stripe\Checkout\Session;
 
+require 'vendor/autoload.php';
+
 class PayloadController extends Controller
 {
-  public function store(Request $request)
+  public function store(Request $request, Response $response)
   {
     // API
     $stripeApiKey = config("env.stripe_api_key");
     Stripe::setApiKey($stripeApiKey);
 
     // header
-    header('Content-Type: application/json');
+    // header('Content-Type: application/json');
 
     // domain（仮ドメイン）
     $domain = "https://deploy-preview-4--liff-nuxt-laravel-ec.netlify.app";
 
     // product
-    $productImage = $request->input("productImage");
-    $productName = $request->input("productName");
-    $productPrice = $request->input("productPrice");
+    // $productImage = $request->input("productImage");
+    // $productName = $request->input("productName");
+    // $productPrice = $request->input("productPrice");
     $productStripePriceApi = $request->input("productStripePriceApi");
 
     // checkout
@@ -38,7 +41,6 @@ class PayloadController extends Controller
       'cancel_url' => $domain . '/cancel',
     ]);
 
-    header("HTTP/1.1 303 See Other");
-    header("Location: " . $checkout_session->url);
+    return $response->withHeader("Location", $checkout_session->url)->withStatus(303);
   }
 }
