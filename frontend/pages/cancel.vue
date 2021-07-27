11074ce9d4236eec23a0761661cdca3f2bed7697
sasaki <skyo.97.admj@gmail.com>
2021-07-27 09:39:45 +0900

    Add a success page and cancel page

---
+++ frontend/pages/cancel.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>商品購入に失敗しました。</p>
+    <p>もう一度ご確認ください。</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ frontend/pages/success.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>ご購入ありがとうございます！</p>
+    <p>ご不明点がありましたらお気軽にご連絡ください。</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
