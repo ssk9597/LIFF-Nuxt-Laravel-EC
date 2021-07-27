2ec17a1666399970ac75c50f86a10957a2799187
sasaki <skyo.97.admj@gmail.com>
2021-07-27 11:34:57 +0900

    Update Payload

--- api/app/Http/Controllers/PayloadController.php
+++ api/app/Http/Controllers/PayloadController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Stripe\Stripe;
 use Stripe\Checkout\Session;
 
 class PayloadController extends Controller
 {
-  public function store(Request $request, Response $response)
+  public function store(Request $request)
   {
     // API
     $stripeApiKey = config("env.stripe_api_key");
@@ -22,9 +21,9 @@ class PayloadController extends Controller
     $domain = "https://deploy-preview-4--liff-nuxt-laravel-ec.netlify.app";
 
     // product
-    // $productImage = $request->input("productImage");
-    // $productName = $request->input("productName");
-    // $productPrice = $request->input("productPrice");
+    $productImage = $request->input("productImage");
+    $productName = $request->input("productName");
+    $productPrice = $request->input("productPrice");
     $productStripePriceApi = $request->input("productStripePriceApi");
 
     // checkout
@@ -39,6 +38,7 @@ class PayloadController extends Controller
       'cancel_url' => $domain . '/cancel',
     ]);
 
-    return $response->withHeader("Location", $checkout_session->url)->withStatus(303);
+    header("HTTP/1.1 303 See Other");
+    header("Location: " . $checkout_session->url);
   }
 }
