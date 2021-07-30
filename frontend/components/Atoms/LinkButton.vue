0f41454677ae6d0fcf14615a21732758ffec41a1
sasaki <skyo.97.admj@gmail.com>
2021-07-30 10:30:06 +0900

    Update css_return to the top page

--- frontend/components/Atoms/LinkButton.vue
+++ frontend/components/Atoms/LinkButton.vue
@@ -10,18 +10,13 @@ export default {};
 
 <style lang="scss" scoped>
 .button {
-  position: relative;
   margin: 20px auto 0 auto;
   width: 200px;
   height: 40px;
   background: $color_yellow;
   border-color: transparent;
   &-btn {
-    position: absolute;
-    width: 100%;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    line-height: 40px;
     font-size: 18px;
     color: $color_black;
     text-decoration: none;
